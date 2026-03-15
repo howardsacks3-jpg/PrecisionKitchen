a34e50dfe447d8f73316b73b223bdc4220febb14
howardsacks3-jpg <howardsacks3@gmail.com>
2026-03-15 14:20:41 +0200

    Create product-page.js

---
+++ js/product-page.js
@@ -0,0 +1,20 @@
+async function loadProduct() {
+  const params = new URLSearchParams(window.location.search);
+  const id = params.get("id");
+  if (!id) return;
+
+  const res = await fetch("data/products.json");
+  const products = await res.json();
+  const p = products.find(x => x.id == id);
+  if (!p) return;
+
+  document.getElementById("product-page").innerHTML = `
+    <img src="${p.image}" width="300">
+    <h1>${p.name}</h1>
+    <p>${p.description}</p>
+    <p>$${p.price}</p>
+    <button onclick='addToCart(${JSON.stringify(p)})'>Add To Cart</button>
+  `;
+}
+
+loadProduct();
