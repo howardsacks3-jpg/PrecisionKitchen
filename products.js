814043b3c154dd432963850e6b5681777ae0115f
howardsacks3-jpg <howardsacks3@gmail.com>
2026-03-15 13:58:07 +0200

    Update products.js

--- products.js
+++ products.js
@@ -1,27 +1,21 @@
 async function loadProducts(){
 
 const res = await fetch("data/products.json")
-
 const products = await res.json()
 
 const grid = document.getElementById("product-grid")
 
-grid.innerHTML = products.map(p=>`
+if(!grid) return
 
-<div class="product-card">
+grid.innerHTML = products.map(p => `
+
+<div class="product">
 
 <a href="product.html?id=${p.id}">
-<img src="${p.image}" loading="lazy">
+<img src="${p.image}" width="200">
 </a>
 
 <h3>${p.name}</h3>
-
-<div class="stars">
-
-★★★★★
-<span>(${p.reviews})</span>
-
-</div>
 
 <p>$${p.price}</p>
 
