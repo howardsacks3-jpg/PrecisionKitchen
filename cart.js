47cf97a50140ef31d76f99fac1c37a87b12fc42e
howardsacks3-jpg <howardsacks3@gmail.com>
2026-03-15 16:06:36 +0200

    Update cart.js

--- cart.js
+++ cart.js
@@ -2,6 +2,7 @@ let cart = JSON.parse(localStorage.getItem("cart")) || []
 
 function saveCart(){
 localStorage.setItem("cart", JSON.stringify(cart))
+updateCartCount()
 }
 
 function addToCart(product){
@@ -15,7 +16,42 @@ cart.push({...product, qty:1})
 }
 
 saveCart()
-
-alert("Added to cart")
+alert(product.name + " added to cart")
 
 }
+
+function updateQuantity(id,delta){
+
+const item = cart.find(i=>i.id===id)
+
+if(!item) return
+
+item.qty+=delta
+
+if(item.qty<=0){
+cart = cart.filter(i=>i.id!==id)
+}
+
+saveCart()
+
+}
+
+function removeFromCart(id){
+
+cart = cart.filter(i=>i.id!==id)
+
+saveCart()
+
+}
+
+function updateCartCount(){
+
+const count = cart.reduce((sum,i)=>sum+i.qty,0)
+
+document.querySelectorAll(".cart-count").forEach(el=>{
+el.innerText = count
+})
+
+}
+
+updateCartCount()
