62323ec556237be0dd31e788f94616c0639e17bc
howardsacks3-jpg <howardsacks3@gmail.com>
2026-03-15 13:59:13 +0200

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,19 +1,21 @@
 let cart = JSON.parse(localStorage.getItem("cart")) || []
 
-function addToCart(product){
-
-const existing = cart.find(i=>i.id===product.id)
-
-if(existing){
-
-existing.qty++
-
-}else{
-
-cart.push({...product,qty:1})
-
+function saveCart(){
+localStorage.setItem("cart", JSON.stringify(cart))
 }
 
-localStorage.setItem("cart",JSON.stringify(cart))
+function addToCart(product){
+
+const existing = cart.find(i => i.id === product.id)
+
+if(existing){
+existing.qty++
+}else{
+cart.push({...product, qty:1})
+}
+
+saveCart()
+
+alert("Added to cart")
 
 }
