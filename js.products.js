250fe9d273c49358d9c056e49d112dd8b64523f3
howardsacks3-jpg <howardsacks3@gmail.com>
2026-03-15 16:16:20 +0200

    Create js.products.js

---
+++ js.products.js
@@ -0,0 +1,31 @@
+async function loadProducts(){
+
+const res = await fetch("data/products.json")
+
+const products = await res.json()
+
+const grid = document.getElementById("product-grid")
+
+grid.innerHTML = products.map(p => `
+
+<div class="product">
+
+<a href="product.html?id=${p.id}">
+<img src="${p.image}" alt="${p.name}">
+</a>
+
+<h3>${p.name}</h3>
+
+<p>$${p.price}</p>
+
+<button onclick='addToCart(${JSON.stringify(p)})'>
+Add to Cart
+</button>
+
+</div>
+
+`).join("")
+
+}
+
+loadProducts()
